91de4a009f4f6a0ff207cdc7d2de4209181ecd01
andreapmacedo <aa.andremacedo@gmail.com>
2022-07-23 14:36:22 -0300

    feito: desafio 1

---
+++ database-selects.sql
@@ -0,0 +1,7 @@
+SELECT * FROM SpotifyClone.tb_user;
+SELECT * FROM SpotifyClone.tb_song;
+SELECT * FROM SpotifyClone.tb_playback_history;
+SELECT * FROM SpotifyClone.tb_plan;
+SELECT * FROM SpotifyClone.tb_followed_artist;
+SELECT * FROM SpotifyClone.tb_album;
+SELECT * FROM SpotifyClone.tb_artist;
--- database.sql
+++ database.sql
@@ -1,10 +1,19 @@
 DROP SCHEMA IF EXISTS SpotifyClone;
 CREATE DATABASE SpotifyClone;
--- USE SpotifyClone;
+
+CREATE TABLE SpotifyClone.tb_plan (
+    id INTEGER auto_increment PRIMARY KEY NOT NULL,
+    plan_name VARCHAR(30) NOT NULL,
+    plan_price DECIMAL(10,2) NOT NULL
+);
 
 CREATE TABLE SpotifyClone.tb_user (
-  id INTEGER auto_increment PRIMARY KEY NOT NULL,
-  user_name VARCHAR(30) NOT NULL
+    id INTEGER auto_increment PRIMARY KEY NOT NULL,
+    user_name VARCHAR(30) NOT NULL,
+    user_age VARCHAR(2) NOT NULL,
+    plan_date DATE NOT NULL,
+    id_plan INT NOT NULL,
+    FOREIGN KEY (id_plan) REFERENCES tb_plan (id)
 );
 
 CREATE TABLE SpotifyClone.tb_artist(
@@ -15,49 +24,35 @@ CREATE TABLE SpotifyClone.tb_artist(
 CREATE TABLE SpotifyClone.tb_album(
     id INTEGER auto_increment PRIMARY KEY NOT NULL,
     album_name VARCHAR(255) NOT NULL,
-    relase_year INTEGER NOT NULL, 
     id_artist INT NOT NULL,
+    relase_year INTEGER NOT NULL, 
     FOREIGN KEY (id_artist) REFERENCES tb_artist (id)
 );
 
 CREATE TABLE SpotifyClone.tb_song (
     id INTEGER auto_increment PRIMARY KEY NOT NULL,
     song_name VARCHAR(255) NOT NULL,
-    song_duration INT,
-    album_id INT,
+    song_duration INT NOT NULL,
     artist_id INT,
+    album_id INT,
     FOREIGN KEY (artist_id) REFERENCES tb_artist (id),
-    FOREIGN KEY (album_id) REFERENCES tb_user (id)
+    FOREIGN KEY (album_id) REFERENCES tb_album (id)
 );
 
 CREATE TABLE SpotifyClone.tb_playback_history(
-    id INTEGER auto_increment PRIMARY KEY NOT NULL,
-    reproduced_date DATETIME NOT NULL,
     id_user INT NOT NULL,
     id_song INT NOT NULL,
+    reproduced_date DATETIME NOT NULL,
+    CONSTRAINT PRIMARY KEY (id_user, id_song),
     FOREIGN KEY (id_user) REFERENCES tb_user (id),
     FOREIGN KEY (id_song) REFERENCES tb_song (id)
 );
   
 CREATE TABLE SpotifyClone.tb_followed_artist(
-    id INTEGER auto_increment PRIMARY KEY NOT NULL,
     id_user INT NOT NULL,
     id_artist INT NOT NULL,
+    CONSTRAINT PRIMARY KEY (id_user, id_artist),
     FOREIGN KEY (id_user) REFERENCES tb_user (id),
     FOREIGN KEY (id_artist) REFERENCES tb_artist (id)
 );
 
-CREATE TABLE SpotifyClone.tb_plan (
-    id INTEGER auto_increment PRIMARY KEY NOT NULL,
-    plan_name VARCHAR(30) NOT NULL,
-    plan_price DECIMAL(10,2) NOT NULL,
-    date DATETIME NOT NULL
-);
-  
-CREATE TABLE SpotifyClone.tb_user_plan(
-    id INTEGER auto_increment PRIMARY KEY NOT NULL,
-    id_user INT NOT NULL,
-    id_plan INT NOT NULL,
-    FOREIGN KEY (id_user) REFERENCES tb_user (id),
-    FOREIGN KEY (id_plan) REFERENCES tb_plan (id)
-);
